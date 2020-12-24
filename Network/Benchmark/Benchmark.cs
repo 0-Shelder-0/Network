9a233a64410e983aab1d09fcb5c769e2563ca1d9
Vlad <vladpiligrimov@gmail.com>
2020-12-24 22:56:39 +0500

    Refactoring

--- Network/Benchmark/Benchmark.cs
+++ Network/Benchmark/Benchmark.cs
@@ -9,69 +9,72 @@ namespace Network.Benchmark
 {
     public static class Benchmark
     {
-        public static void Run(IGraph graph, string directoryPath, int numberItems, int numberRepetitions)
+        public static void Run(IGraph graph, string directoryPath, int numberOfItems, int numberRepetitions)
         {
+            numberOfItems++;
             var allResults = new List<(string, List<double>)>();
-            var nodeNumbers = GetNodeNumbers(numberItems);
-            var edges = GetEdges(numberItems);
+            var nodeNumbers = GetNodeNumbers(numberOfItems);
+            var edges = GetEdges(numberOfItems, numberOfItems);
+            var rnd = new Random();
+            graph.Clear();
 
             for (var i = 0; i < numberRepetitions; i++)
             {
-                allResults.AddRange(MeasureTwoMethods(graph, nodeNumbers, "AddNode", "ContainsNode"));
-                allResults.AddRange(MeasureTwoMethods(graph, edges, "Connect", "IsConnect"));
+                var numberEdges = rnd.Next(1, numberOfItems * (numberOfItems - 1) / 2);
+                allResults.AddRange(MeasurePairMethods(graph, nodeNumbers, "AddNode", "ContainsNode"));
+                allResults.AddRange(MeasurePairMethods(graph, edges, "Connect", "IsConnect"));
                 allResults.Add(MeasureMethod(graph, edges, "Disconnect"));
-                ConnectAllEdges(graph, edges);
+                ConnectEdges(graph, GetEdges(numberOfItems, numberEdges));
                 allResults.Add(MeasureMethod(graph, nodeNumbers, "RemoveNode"));
+                allResults[^1].Item2.Reverse();
             }
 
             var results = CombineResults(allResults);
             WriteToFiles(directoryPath, results);
         }
 
-        private static IEnumerable<(string, List<double>)> MeasureTwoMethods(IGraph graph,
-                                                                             IEnumerable<object[]> parametersEnumerable,
-                                                                             string firstName,
-                                                                             string secondName)
+        private static IEnumerable<(string, List<double>)> MeasurePairMethods(IGraph graph,
+                                                                              IEnumerable<object[]> methodsParameters,
+                                                                              string firstMethodName,
+                                                                              string secondMethodName)
         {
             var measurer = new Measurer.Measurer();
-            var firstResults = new List<double>();
-            var secondResults = new List<double>();
-            foreach (var parameters in parametersEnumerable)
+            var firstMethodResults = new List<double>();
+            var secondMethodResults = new List<double>();
+            foreach (var parameters in methodsParameters)
             {
-                var result = measurer.Measure(graph, firstName, parameters);
-                firstResults.Add(result);
+                var result = measurer.Measure(graph, firstMethodName, parameters);
+                firstMethodResults.Add(result);
 
-                result = measurer.Measure(graph, secondName, parameters);
-                secondResults.Add(result);
+                result = measurer.Measure(graph, secondMethodName, parameters);
+                secondMethodResults.Add(result);
             }
-            return new[] {(firstName, firstResults), (secondName, secondResults)};
+            return new[] {(firstMethodName, firstMethodResults), (secondMethodName, secondMethodResults)};
         }
 
         private static (string, List<double>) MeasureMethod(IGraph graph,
-                                                            IEnumerable<object[]> parametersEnumerable,
+                                                            IEnumerable<object[]> methodParameters,
                                                             string methodName)
         {
             var measurer = new Measurer.Measurer();
             var result = new List<double>();
-            foreach (var parameters in parametersEnumerable)
+            foreach (var parameters in methodParameters)
             {
                 result.Add(measurer.Measure(graph, methodName, parameters));
             }
             return (methodName, result);
         }
 
-        private static IEnumerable<(string, List<double>)> CombineResults(
-            IReadOnlyCollection<(string, List<double>)> results)
+        private static IEnumerable<(string, List<double>)> CombineResults(List<(string, List<double>)> results)
         {
             var combineResults = new List<(string, List<double>)>();
-            var names = new HashSet<string>(results.Select(item => item.Item1));
-            foreach (var name in names)
+            var namesOfMethods = new HashSet<string>(results.Select(item => item.Item1));
+            foreach (var name in namesOfMethods)
             {
                 combineResults.Add((name, new List<double>()));
-                var methodResults = results
-                                   .Where(tuple => string.Compare(tuple.Item1, name, StringComparison.Ordinal) == 0)
-                                   .ToList();
-                for (var i = 1; i < methodResults.FirstOrDefault().Item2.Count; i++)
+                var methodResults = results.Where(tuple => name.Equals(tuple.Item1)).ToList();
+                var numberOfResults = methodResults.FirstOrDefault().Item2.Count;
+                for (var i = 1; i < numberOfResults; i++)
                 {
                     var avg = methodResults.Average(item => item.Item2[i]);
                     combineResults[^1].Item2.Add(avg);
@@ -80,7 +83,7 @@ namespace Network.Benchmark
             return combineResults;
         }
 
-        private static void ConnectAllEdges(IGraph graph, IEnumerable<object[]> edges)
+        private static void ConnectEdges(IGraph graph, IEnumerable<object[]> edges)
         {
             foreach (var parameters in edges)
             {
@@ -93,28 +96,26 @@ namespace Network.Benchmark
             }
         }
 
-        private static List<object[]> GetNodeNumbers(int numberItems)
+        private static List<object[]> GetNodeNumbers(int count)
         {
-            return Enumerable.Range(0, numberItems)
+            return Enumerable.Range(0, count)
                              .Select(item => new object[] {item})
                              .ToList();
         }
 
-        private static List<object[]> GetEdges(int count)
+        private static List<object[]> GetEdges(int maxValue, int count)
         {
             var set = new HashSet<(int, int)>();
             var rnd = new Random();
             while (set.Count != count)
             {
-                var edge = (rnd.Next(0, count), rnd.Next(0, count));
-                if (edge.Item1 >= edge.Item2)
+                var edge = (rnd.Next(0, maxValue), rnd.Next(0, maxValue));
+                if (edge.Item1 < edge.Item2)
                 {
-                    continue;
+                    set.Add(edge);
                 }
-                set.Add(edge);
             }
-            return set.Select(tuple => new object[] {tuple.Item1, tuple.Item2})
-                      .ToList();
+            return set.Select(tuple => new object[] {tuple.Item1, tuple.Item2}).ToList();
         }
 
         private static void WriteToFiles(string directoryPath, IEnumerable<(string, List<double>)> results)
@@ -122,12 +123,12 @@ namespace Network.Benchmark
             foreach (var (methodName, methodResults) in results)
             {
                 var path = Path.Combine(directoryPath, $"{methodName}.txt");
-                using (var fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write))
+                using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
                 {
                     fileStream.StreamWriteLine(methodName);
                     foreach (var result in methodResults)
                     {
-                        fileStream.StreamWriteLine(result.ToString(CultureInfo.InvariantCulture));
+                        fileStream.StreamWriteLine(result.ToString(CultureInfo.CurrentCulture));
                     }
                 }
             }
--- Network/Program.cs
+++ Network/Program.cs
@@ -4,13 +4,13 @@
     {
         private static void Main()
         {
-            RunBenchmark(5);
+            RunBenchmark(50);
         }
 
         private static void RunBenchmark(int numberRepetitions)
         {
             const string path = "C:/Users/Vlad/Desktop/Tests";
-            Benchmark.Benchmark.Run(new Graph.Graph(), path, 1001, numberRepetitions);
+            Benchmark.Benchmark.Run(new Graph.Graph(), path, 250, numberRepetitions);
         }
     }
 }
